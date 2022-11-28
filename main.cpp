82e7dc5fd3b377f77f5ced990b8b012615d8e8aa
angelicodm <72331555+angelicodm@users.noreply.github.com>
2022-11-27 21:56:59 -0800

    Create main.cpp
    Skeleton/ first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	bool validInput = false;
+	string selectedFile;
+	
+	while(!validInput)
+	{
+		cout << "Welcome to the Feature Selection Algorithm." << endl;
+		cout << "Type in the name of the file to test: ";
+	}
+	
+	return 0;
+}
