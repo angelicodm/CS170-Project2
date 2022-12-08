e866f7440e9ac4f9bc01f5b9738d2ab7e607df36
angelicodm <72331555+angelicodm@users.noreply.github.com>
2022-12-08 14:42:24 -0800

    added nearest neighbor and accuracy(leave one out) function

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <cmath>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 vector <vector <long double>> dataSet; //separates the data set into individual rows
 vector<long double> featureSet; //the columns in the data set
+
+/* NOTE:
+ * Assigned datasets: small53 and large105
+ * From the key provided in the email:
+ * 		small53 features: 3 6 4		small53 accuracy: 0.942
+ * 		large105 features: 28 18 9	large105 accuracy: 0.942
+ */
 
 void loadData(string selectedFile)
 {
@@ -37,19 +47,75 @@ void loadData(string selectedFile)
 	}
 }
 
-int nearestNeighbor()
+double nearestNeighbor(vector <vector <long double>> data, vector<long double> instance)
 {
+	long double nearestNeighborDistance = INFINITY;
+	long double distance, objectToClassify;
+	long double classLabel = instance[0];
 	
+	for(int i = 0; i < data[0].size(); i++)
+	{
+		distance = 0.0; //distance has to be assigned to 0 here or it won't work
+		for(int j = 0; j < data.size(); j++)
+		{
+			if(j > 0) //wont work if j ever equals 0
+			{
+				//distance = sqrt((sum(object_to_classify -data(k,2:end)).^2));
+				objectToClassify = data[j][i];
+				distance += pow((objectToClassify - instance[j]), 2);
+			}
+		}
+		
+		if(distance < nearestNeighborDistance) //if closer neighbor was found, updates nearestNeighborDistance and classLabel
+		{
+			nearestNeighborDistance = distance;
+			classLabel = (int)data[0][i];
+		}
+	}
+	return classLabel;
 }
 
-double accuracy()
+double accuracy(vector <vector <long double>> dataSet) //nearest neighbor should be separate to keep it simple
 {
+	double accuracy;
+	double correctlyClassified = 0;
+	vector <vector <long double>> data;
+	vector <long double> instance;
+	vector <long double> copy; //used to copy entries from accData to instance
 	
+	for(int i = 0; i < dataSet[0].size(); i++)
+	{
+		for(int j = 0; j < dataSet.size(); j++)
+		{
+			copy = dataSet[j]; //copies entire column of features
+			instance.push_back(copy[i]); // copies exact feature from instance we want
+			copy.erase(copy.begin() + i); //removes the individual feature from the instance we want (leaves one out)
+			data.push_back(copy); //copies rest of feature we want from all instances into data for comparison
+		}
+		
+		if(nearestNeighbor(data, instance) == instance[0])
+		{
+			correctlyClassified++;
+		}
+		data.clear();
+		instance.clear();
+	}
+	
+	accuracy = (correctlyClassified / data[0].size()) * 100;
+	
+	return accuracy;
 }
 
-void forwardSelection()
+void forwardSelection(vector< vector< long double> dataSet)
 {
-	
+	vector<int> bestFeatures; //only used to store final set of best features for output
+    vector<int> currentBest;  //updates every iteration with the best feature(s)
+    vector<int> copyBest; //purely used to copy stored data to update currentBest after the nested for loop
+    vector<vector<long double>> dataCopy; //a modified copy of dataSet to be used to find the accuracy of the current selected features
+    dataCopy.push_back(dataSet.at(0)); //add class labels from dataSet to dataCopy
+    vector<int> currentFeatures; //updates every iteration with current best features and next feature to be tested
+    double acc, localAcc, bestAcc; //used to keep track of varying accuracies
+    
 }
 
 void backwardElimination()
